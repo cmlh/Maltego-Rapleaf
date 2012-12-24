c8c2bfa6099b2d666fee3feb1bd5780c5c21da93
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-12-24 11:17:01 +1100

    Add test.sh

---
+++ Local_Transforms/t/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+#
+# This is a workaround until I convert this to TAP
+#
+# christian.heinrich@cmlh.id.au
+# 
+../Utilities_API/from_affiliation_facebook-to_rapleaf_gender.pl "Christian Heinrich" "person.name=Christian Heinrich#network=Facebook#uid=100002552265389#profile_url=http://www.facebook.com/cmlh.id.au"
+../Utilities_API/from_email-to_rapleaf_name.pl "Christian Heinrich" "christian.heinrich@cmlh.id.au"
