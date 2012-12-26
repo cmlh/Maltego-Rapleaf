27c1e384364bb3bb6721a0af299f063e98de1185
Christian Heinrich <christian.heinrich@cmlh.id.au>
2012-12-26 20:34:16 +1100

    Feature refactor of ./Local_Transforms dirs

--- Build/create_release.sh
+++ Build/create_release.sh
@@ -5,7 +5,7 @@
 # christian.heinrich@cmlh.id.au
 #
 cd ..
-tar -cvf ./Releases/Maltego-Rapleaf-$1.tar ./Local_Transforms/etc/*.conf ./Local_Transforms/*.pl
+tar -cvf ./Releases/Maltego-Rapleaf-$1.tar ./Local_Transforms/etc/*.conf ./Local_Transforms/Perl-Maltego/*.pl ./Local_Transforms/Personalization_API/*.pl ./Local_Transforms/Utilities_API/*.pl
 tar -rvf ./Releases/Maltego-Rapleaf-$1.tar ./Entities/*.mtz
 gzip ./Releases/Maltego-Rapleaf-$1.tar
 cd Build
